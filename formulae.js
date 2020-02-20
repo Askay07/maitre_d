7ceba69ba5f9f36471693e810165df719daa91f6
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2020-02-20 17:01:44 -0600

    .

--- formulae.js
+++ formulae.js
@@ -7,9 +7,9 @@ var taxA = Math.round(total * (5.5/100));
 var tip = Math.round(total * tipR);
 var Gtotal = Math.round(total + tip + taxA);
 
-document.getElementById("uno").innerHTML = taxA
-document.getElementById("dos").innerHTML = tip
-document.getElementById("tres").innerHTML = Gtotal
+document.getElementById("tres").innerHTML = taxA
+document.getElementById("quatro").innerHTML = tip
+document.getElementById("cinco").innerHTML = Gtotal
 
 
 }
