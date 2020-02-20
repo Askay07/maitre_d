ccee1fb2cf0419d484cd1ef45b1d7905de87b091
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2020-02-20 17:35:45 -0600

    Update formulae.js

--- formulae.js
+++ formulae.js
@@ -1,19 +1,20 @@
 function calculate(){
 
-var total = document.getElementsById("uno").value;
-var tipR = document.getElementsById("dos").value;
-
-var taxA = Math.round(total * (5.5/100));
-var tip = Math.round(total * tipR);
-var Gtotal = Math.round(total + tip + taxA);
-
-document.getElementById("tres").innerHTML = taxA
-document.getElementById("quatro").innerHTML = tip
-document.getElementById("cinco").innerHTML = Gtotal
-
-
-}
-
-document.getElementById("lamo").onclick = function() {
-calculate();
-};
+    var total = document.getElementsById("uno").value;
+    var tipR = document.getElementsById("dos").value;
+    
+    var taxA = total * (5.5/100);
+    var tipA = (total * tipR);
+    var Gtotal = (total + tipA + taxA);
+    
+    document.getElementById("tres").innerHTML = taxA;
+    document.getElementById("quatro").innerHTML = tipA;
+    document.getElementById("cinco").innerHTML = Gtotal;
+    }
+    
+    document.getElementById("lamo").onclick = function() {
+        
+    calculate();
+    
+    };
+    
