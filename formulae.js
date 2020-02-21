9a5170fab519b28431a2c87ff3e1804b2e5d6b72
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2020-02-21 01:04:16 -0600

    1

--- formulae.js
+++ formulae.js
@@ -1,12 +1,14 @@
 <script>
-  function calculateion() {
   
- var total = document.getElementById("uno").value;
+  function calculate() {
+    
+  var total = document.getElementById("uno").value;
   var tipR = document.getElementById("dos").value;
   
   var taxA = (parseFloat(total) * (5.5 / 100));
   var tip = (parseFloat(total) * parseFloat(tipR));
   var Gtotal = (parseFloat(total) + parseFloat(tip) + parseFloat(taxA));
+
   if (!isNaN(Gtotal)) {
 
     document.getElementById("tres").innerHTML = taxA;
